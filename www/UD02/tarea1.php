341cabf1708e217366bc5db709071dc5577c4b67
Mar Castro <A19MariaCC@iessanclemente.net>
2023-10-07 18:19:44 +0200

    Tarea2 UD2

--- www/UD02/tarea1.php
+++ www/UD02/tarea1.php
@@ -2,6 +2,6 @@
 /*En la siguiente instrucción, aunque el código se ejecuta igualmente, se recomienda utilizar comillas dobles */
     echo '¿Cómo estás?<br/>'; //Cerramos el ? y además añadimos un salto de línea con la etiqueta <br>
     echo 'Estoy bien, gracias';
-
+   
 //Eliminamos el segundo ? en la siguiente instrucción para cerrar el archivo PHP
 ?>
