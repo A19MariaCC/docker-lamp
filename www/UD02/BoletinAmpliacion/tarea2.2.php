df82160c35dd0343b66047a569e170f415af8716
Mar Castro <A19MariaCC@iessanclemente.net>
2023-10-11 12:47:32 +0200

    Tarea 2.2 Boletín ampliación

---
+++ www/UD02/BoletinAmpliacion/tarea2.2.php
@@ -0,0 +1,40 @@
+<?php
+/*Escribir un programa que lea la entrada de la hora de un día en notación 14 horas y muestre la respuesta en notación de 12 horas. 
+Por ejemplo, si introducimos 18:05 debe proporcionar como salida 06:05 PM.
+*/
+
+function notacionHora($hora){ 
+   $h2;
+   $m2;
+   $mensaje;
+    if(strlen($hora) == 5){
+        $h = substr($hora,0,2);
+        //echo $h;
+        $h1 = intval($h);
+        $m2 = substr($hora,3,5);
+        if($h1 > 23){
+            echo "La hora introducida es incorrecta<br />";
+        }else{
+            if($h1 > 11){
+                $mensaje = "PM";
+            }else{
+                $mensaje = "AM";
+            }
+        }
+        if($h1 > 12){
+            $h1 = $h1 -12;
+            
+        }
+        $h2 = strval($h1);
+    }
+    
+
+	
+	return $h2.":".$m2." ".$mensaje;
+} 
+   
+//Llamamos a la función
+echo notacionHora("11:05");
+
+
+?>
