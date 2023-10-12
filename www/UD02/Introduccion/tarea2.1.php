29a18fd5cb8913e59095475cc9179f328d22ea87
Mar Castro <A19MariaCC@iessanclemente.net>
2023-10-12 10:42:45 +0200

    Tarea2.1 Introduccion

---
+++ www/UD02/Introduccion/tarea2.1.php
@@ -0,0 +1,7 @@
+<?php //Eliminamos el espacio en blanco porque sino no lo reconoce como código PHP
+/*En la siguiente instrucción, aunque el código se ejecuta igualmente, se recomienda utilizar comillas dobles */
+    echo '¿Cómo estás?<br/>'; //Cerramos el ? y además añadimos un salto de línea con la etiqueta <br>
+    echo 'Estoy bien, gracias';
+   
+//Eliminamos el segundo ? en la siguiente instrucción para cerrar el archivo PHP
+?>
