e6c96ced43a111465c6eb17dd21834c071f100ae
Mar Castro <A19MariaCC@iessanclemente.net>
2023-10-12 10:43:51 +0200

    Tarea2.6 Introduccion

---
+++ www/UD02/Introduccion/tarea2.6.php
@@ -0,0 +1,15 @@
+<?php
+//Usando la instrucción echo crea un programa en PHP que muestre el mensaje: Hola, Mundo! Optativo. Muéstralo en cursiva.
+
+echo "<i>Hola, Mundo!</i><br />";
+
+/*Crea un programa en PHP que guarde en una variable tu nombre y lo muestre en negrita 
+formando el siguiente mensaje: Bienvenido tunombre. */
+
+$nombre = "Mar"; //Variable que almacena mi nombre
+//Mostramos el mensaje en negrita
+echo "<strong>Bienvenida ".$nombre."</strong>";
+
+
+
+?>
